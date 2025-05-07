878bee3f727c9ff38dcec78f74fdfeee0932e036
Kirill <scriptythememer@gmail.com>
2025-05-07 15:49:40 +0300

    fist combo period

--- Code/Player/Action/Fist.cs
+++ Code/Player/Action/Fist.cs
@@ -2,10 +2,18 @@ public class Fist : IBrawlerAction {
     public BrawlerComponent Player {get; set;} 
     public string Name {get;} = "Fist";
 
-    public float Duration {get; set;}
+    public float Duration {get; set;} = 1;
     public float LastTime {get; set;}
 
     public void OnStart() {}
-    public void OnUpdate() {}
+    public void OnUpdate() {
+        var time = Time.Now - LastTime;
+
+        //combo period
+        if (time > 0.65 && Player.CanTraverseTree == false) {
+            Player.CanTraverseTree = true;
+        }
+    }
+
     public void OnStop() {}
 }
